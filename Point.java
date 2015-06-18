3001dc3f5a635951ced64e664a23ff49b83af7dc
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-18 20:37:47 +0200

    Klasse für Punkte erstellt

---
+++ Point.java
@@ -0,0 +1,18 @@
+
+/**
+ * A single Point.
+ */
+public class Point
+{
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for creating Point objects
+     */
+    public Point()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+}
