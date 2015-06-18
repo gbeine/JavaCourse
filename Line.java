fcc9f22bd98fe801ece9de8beb536763c9681781
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-18 21:13:01 +0200

    Klasse Linie anlegen

---
+++ Line.java
@@ -0,0 +1,36 @@
+
+/**
+ * A simple Line.
+ */
+public class Line
+{
+    private Point p1;
+    private Point p2;
+    
+    /**
+     * Constructor for creating Line objects.
+     */
+    public Line(Point p1, Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+    
+    public int getX(int p)
+    {
+        if (p == 1) {
+            return this.p1.getX();
+        } else {
+            return this.p2.getX();
+        }
+    }    
+
+    public int getY(int p)
+    {
+        if (p == 1) {
+            return this.p1.getY();
+        } else {
+            return this.p2.getY();
+        }
+    }
+}
---
+++ LineTest.java
@@ -0,0 +1,22 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * A test class for Line objects.
+ */
+public class LineTest
+{
+    @Test
+    public void createLine()
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 5);
+        Line l = new Line(p1, p2);
+        assertEquals(1, l.getX(1));
+        assertEquals(2, l.getY(1));
+        assertEquals(1, l.getX(2));
+        assertEquals(5, l.getY(2));
+    }
+}
