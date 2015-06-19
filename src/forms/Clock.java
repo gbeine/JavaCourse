82490070068e22c140e9868dcacb9f68a60d0448
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-19 14:16:27 +0200

    Uhr nutzt immutable Punkt

--- src/forms/Clock.java
+++ src/forms/Clock.java
@@ -16,13 +16,14 @@ public class Clock
 
         clock.add(c);
 
+        Point p1 = new Point(300, 390);
+        Point p2 = new Point(300, 400);
+
         for ( int i = 0; i < 12; i++) {
-            Point p1 = new Point(300, 390);
-            p1.rotate(center, 30*i);
-            Point p2 = new Point(300, 400);
-            p2.rotate(center, 30*i);
             Line l = new Line(p1, p2);
             clock.add(l);
+            p1 = p1.rotate(center, 30);
+            p2 = p2.rotate(center, 30);
         }
 
         clock.draw(wb);
