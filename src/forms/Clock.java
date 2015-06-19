ff21aaf67fd140f33796cb01c8e4a874b9eaf60a
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-19 14:27:23 +0200

    Rotation des Zeigers sichtbar machen

--- src/forms/Clock.java
+++ src/forms/Clock.java
@@ -33,6 +33,11 @@ public class Clock
 
         for (int i = 0; i < 20; i++) {
         	pointer.rotate(center, -6);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+            	System.err.println(ex.getStackTrace());
+            }
         }
     }
 }
