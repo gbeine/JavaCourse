0211a8f22f4e8507783a943654d342d8e80c91a2
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-21 17:09:05 +0200

    Raumschiff in eigene Klasse refactored, Raumschiff ist ein Sprite

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,33 +4,14 @@ import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 import teaching.WhiteBoard;
-import forms.Figure;
-import forms.Point;
-import forms.Rectangle;
-import forms.Triangle;
 
 public class Game {
 
 	public static void main(String[] args) {
 
         Cockpit cockpit = createCockpit();
-
 		WhiteBoard wb = new WhiteBoard();
-
-		Figure spaceShip = new Figure();
-
-		Point p1 = new Point(100, 100);
-		Point p2 = new Point(75, 50);
-		Point p3 = new Point(125, 50);
-        Triangle d = new Triangle(p1, p2, p3);
-        Point p4 = new Point(75, 0);
-        Rectangle r = new Rectangle(p4, p3);
-
-        spaceShip.add(d);
-        spaceShip.add(r);
-
-        spaceShip.draw(wb);
-
+		SpaceShip ship = new SpaceShip(wb);
 	}
 
 	private static Cockpit createCockpit() {
---
+++ src/game/SpaceShip.java
@@ -0,0 +1,47 @@
+package game;
+
+import teaching.WhiteBoard;
+import forms.Figure;
+import forms.Form;
+import forms.Point;
+import forms.Rectangle;
+import forms.Triangle;
+
+public class SpaceShip implements Sprite {
+
+	private final WhiteBoard wb;
+	private Figure spaceShip;
+
+	public SpaceShip(WhiteBoard wb) {
+		this.wb = wb;
+		initialize();
+	}
+
+	private void initialize() {
+		this.spaceShip = new Figure();
+
+		Point p1 = new Point(100, 100);
+		Point p2 = new Point(75, 50);
+		Point p3 = new Point(125, 50);
+        Triangle d = new Triangle(p1, p2, p3);
+        Point p4 = new Point(75, 0);
+        Rectangle r = new Rectangle(p4, p3);
+
+        this.spaceShip.add(d);
+        this.spaceShip.add(r);
+
+        this.spaceShip.draw(wb);
+	}
+
+	@Override
+	public void setDirection(int direction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSpeed(int speed) {
+		// TODO Auto-generated method stub
+
+	}
+}
