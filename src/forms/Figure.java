0245cc62b08e2dbb7f61c2a92eba4421afc0714a
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-21 16:37:38 +0200

    Lösung 12. Aufgabe, Aus Figure eine Form machen

--- src/forms/Figure.java
+++ src/forms/Figure.java
@@ -1,49 +1,61 @@
 package forms;
 
-
 import java.util.Set;
 import java.util.HashSet;
 
-public class Figure
-{
+public class Figure implements Form {
 
-    private Set<Form> forms;
+	private Set<Form> forms;
 
-    public Figure()
-    {
-        forms = new HashSet<Form>();
-    }
+	public Figure() {
+		forms = new HashSet<Form>();
+	}
 
-    public void add(Form f)
-    {
-        forms.add(f);
-    }
+	public void add(Form f) {
+		forms.add(f);
+	}
 
-    public void move(int dx, int dy)
-    {
-        for(Form f : forms) {
-            f.move(dx, dy);
-        }
-    }
+	public void move(int dx, int dy) {
+		for (Form f : forms) {
+			f.move(dx, dy);
+		}
+	}
 
-    public void rotate(Point center, double alpha)
-    {
-        for(Form f : forms) {
-            f.rotate(center, alpha);
-        }
-    }
+	public void rotate(Point center, double alpha) {
+		for (Form f : forms) {
+			f.rotate(center, alpha);
+		}
+	}
 
-    public void draw(teaching.WhiteBoard wb)
-    {
-        for(Form f : forms) {
-            f.draw(wb);
-        }
-    }
+	public void draw(teaching.WhiteBoard wb) {
+		for (Form f : forms) {
+			f.draw(wb);
+		}
+	}
 
-    public void remove()
-    {
-        for(Form f : forms) {
-            f.remove();
-        }
-    }
+	public void remove() {
+		for (Form f : forms) {
+			f.remove();
+		}
+	}
+
+	@Override
+	public void rotate(double alpha) {
+		final Point center = this.getCenter();
+		for (Form f : forms) {
+			f.rotate(center, alpha);
+		}
+	}
+
+	@Override
+	public Point getCenter() {
+		double x = 0, y = 0;
+
+		for (Form form : this.forms) {
+			x += form.getCenter().getX();
+			y += form.getCenter().getY();
+		}
+
+		return new Point(x / this.forms.size(), y / this.forms.size());
+	}
 }
