19787aa55671307786b6adabdee0a1ee673e3279
Gerrit Beine <gerrit.beine@gmx.de>
2015-06-21 18:22:51 +0200

    Asteroiden an zufälligen Stellen erscheinen lassen

--- src/game/AsteroidFactory.java
+++ src/game/AsteroidFactory.java
@@ -6,13 +6,28 @@ import forms.Point;
 
 public class AsteroidFactory {
 
+
 	public static int COUNT = 10;
+
+	private static final int RADIUS_FACTOR = 10;
+	private static final int RADIUS_MINIMUM = 10;
+
+    private static final int X_FACTOR = 600;
+    private static final int X_POSITION = 30;
+
+    private static final int Y_FACTOR = 50;
+    private static final int Y_POSITION = 500;
 
 	public static void createAsteroids(WhiteBoard wb) {
 
 		for (int i = 0; i < COUNT; i++) {
-			Point center = new Point();
-			Circle asteroid = new Circle(center, 10);
+            int radius = RADIUS_MINIMUM + (int) (Math.random() * RADIUS_FACTOR);
+
+            int x = X_POSITION + (int) (Math.random() * X_FACTOR);
+            int y = Y_POSITION + (int) (Math.random() * Y_FACTOR);
+			Point center = new Point(x, y);
+
+			Circle asteroid = new Circle(center, radius);
 			asteroid.draw(wb);
 		}
 
